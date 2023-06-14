6c3233142193005ab25e898621a2de836498c9f8
Tomasz P <tony1984pl@gmail.com>
2023-06-14 20:25:10 +0200

    ZADANIA

--- TypyDanych/Program.cs
+++ TypyDanych/Program.cs
@@ -6,6 +6,7 @@ namespace Zadanie1
     {
         static void Main(string[] args)
         {
+            //ZADANIE 1
             string Name = "Tomasz";
             string LastName = "Pszeniczka";
             int Age = 38;
@@ -19,8 +20,14 @@ namespace Zadanie1
             Console.WriteLine($"Płeć: {Sex}");
             Console.WriteLine($"PESEL: {Pesel}");
             Console.WriteLine($"Numer pracownika: {EmployeeNumber}");
+            Console.WriteLine();
 
+            //ZADANIE 2
+            char Zmienna1 = 'A';
+            char Zmienna2 = 'B';
+            char Zmienna3 = 'C';
 
+            Console.WriteLine($"{Zmienna3}{Zmienna2}{Zmienna1}");
         }
     }
 }
