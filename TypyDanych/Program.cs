afdd0cef484ad0bba585d0160b7249003bddbf86
Tomasz P <tony1984pl@gmail.com>
2023-06-14 21:22:16 +0200

    Zadania 1-3

--- TypyDanych/Program.cs
+++ TypyDanych/Program.cs
@@ -28,6 +28,25 @@ namespace Zadanie1
             char Zmienna3 = 'C';
 
             Console.WriteLine($"{Zmienna3}{Zmienna2}{Zmienna1}");
+
+            //ZADANIE 3
+
+            double A, B, D ;
+            Console.WriteLine("Program obliczający długość przekątnej prostokąta");
+            Console.WriteLine();
+
+            Console.WriteLine("Podaj długość boku a ");
+            double.TryParse(Console.ReadLine(), out A);
+
+            Console.WriteLine("Podaj długość boku b ");
+            double.TryParse(Console.ReadLine(), out B);
+
+            A = Math.Pow(A, 2);
+            B = Math.Pow(B, 2);
+            D = A+B;
+           
+            Console.WriteLine($"Długość przekątnej prostokąta wynosi:{Math.Sqrt(D)} ");
+
         }
     }
 }
